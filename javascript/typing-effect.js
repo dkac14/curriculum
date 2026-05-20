9f7a232d075388823a5e5e3687825aed6812f4da
Domenica Katherine Amores Cedeño <domenicaamores2006@gmail.com>
2026-05-20 14:55:11 -0500

    Practica#3

--- javascript/typing-effect.js
+++ javascript/typing-effect.js
@@ -1,9 +1,9 @@
-const nombre = "Domenica Katherine Amores Cedeño";
+const nombre = "Hola, soy Dome 👋";
 let i = 0;
 
 function effect(){
     if(i < nombre.length){
-        document.getElementById("nombre").textContent += nombre.charAt(i);
+        document.getElementById("greeting").textContent += nombre.charAt(i);
         i++
         setTimeout(effect, 100);
 
